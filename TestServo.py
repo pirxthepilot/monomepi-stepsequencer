f0034b298acad09716c464113cad6240edbe85b1
Joon Guillen <joon@modulogeek.com>
2016-07-31 08:03:49 -0500

    cleanup and README

--- TestServo.py
+++ TestServo.py
@@ -1,17 +1,5 @@
-from servo2 import Servo
-from servocontrol import ServoControl
+from servo_step import Servo
 from time import sleep
-
-STEP_TIME = '10'    # in milliseconds
-STEP_AMT = '1'
-RIGHT_X = '5'
-RIGHT_Y = '7'
-LEFT_X = '2'
-LEFT_Y = '7'
-SWEEP_X = '4'
-SWEEP_Y = '7'
-EXIT_X = '7'
-EXIT_Y = '7'
 
 
 # INIT
@@ -23,38 +11,34 @@ a.open_servo()
 # MAIN
 #
 
-servothreads = []
-c1 = ServoControl(a, '4')
-d1 = ServoControl(a, '5')
-e1 = ServoControl(a, '6')
-f1 = ServoControl(a, '7')
-g1 = ServoControl(a, '8')
-a1 = ServoControl(a, '9')
-#b1 = ServoControl(a, '10')
-#c2 = ServoControl(a, '11')
-servothreads.append(c1)
-servothreads.append(d1)
-servothreads.append(e1)
-servothreads.append(f1)
-servothreads.append(g1)
-servothreads.append(a1)
-#servothreads.append(b1)
-#servothreads.append(c2)
+s1 = '00000101'
+s2 = '00000110'
 
-for st in servothreads:
-    st.start()
+s3 = ['0' for i in range(8)]
+
+s3[0] = '00000101'
+s3[1] = '00001010'
+s3[2] = '00010100'
+s3[3] = '0'
+s3[4] = '1'
+s3[5] = '0'
+s3[6] = '0'
+s3[7] = '1'
+
 
 while True:
     try:
-        for st in servothreads:
-            st.hit = True
-        sleep(0.5)
+        for i in range(8):
+        #a.move(s1)
+        #print s1
+        #sleep(1)
+            new = ''.join(list(reversed(s3[i])))
+            a.move(format(int(new, 2), '02x'))
+        print new
+        sleep(1)
     except KeyboardInterrupt:
-        for st in servothreads:
-            st.exit_flag = True
-            print "\nExit servo " + st.servo
+        print "\nExit"
         break
-
 
 a.close()
 print "Done."
